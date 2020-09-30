bc6c50cd702c940c930ad708f5e11470987e4fb9
gokulrajanpillai <gokulrajanpillai@gmail.com>
2020-09-30 19:33:26 +0200

    Removed config parameter from print, get_sq functions. Assigned default config value as objects config in whose_turn

--- tictactoe.py
+++ tictactoe.py
@@ -10,16 +10,18 @@ class Board:
         config = config.replace('_', ' ')
         self.config = config
 
-    def get_sq(self, x, y, config):
-        return config[(3 - y) * 3 + x - 1]
+    def get_sq(self, x, y):
+        return self.config[(3 - y) * 3 + x - 1]
 
-    def whose_turn(self, config):
+    def whose_turn(self, config=None):
+        if config is None:
+            config = self.config
         c = Counter(config)
         return 'X' if c['X'] == c['O'] else 'O'
 
     def make_move(self, difficulty='easy', player=None):
         if player is None:
-            player = self.whose_turn(self.config)
+            player = self.whose_turn()
         opponent = 'O' if player == 'X' else 'X'
         if difficulty == 'user':
             while True:
@@ -35,7 +37,7 @@ class Board:
                 else:
                     print('Coordinates should be from 1 to 3!')
                     continue
-                if self.get_sq(x, y, self.config) != ' ':
+                if self.get_sq(x, y) != ' ':
                     print('This cell is occupied! Choose another one!')
                     continue
                 break
@@ -65,12 +67,12 @@ class Board:
                     p = random.randrange(9)
                 self.config = self.config[:p] + player + self.config[p + 1:]
 
-    def print(self, config):
+    def print(self):
         print('---------')
         for y_coordinate in [3, 2, 1]:
             print('|', end=' ')
             for x_coordinate in [1, 2, 3]:
-                print(self.get_sq(x_coordinate, y_coordinate, config), end=' ')
+                print(self.get_sq(x_coordinate, y_coordinate), end=' ')
             print('|')
         print('---------')
 
@@ -135,15 +137,15 @@ class Board:
 
 def play(player1='easy', player2='easy'):
     board = Board()
-    board.print(board.config)
+    board.print()
 
     while board.get_state() == 'Game not finished':
         board.make_move(player1, 'X')
-        board.print(board.config)
+        board.print()
         if board.get_state() != 'Game not finished':
             break
         board.make_move(player2, 'O')
-        board.print(board.config)
+        board.print()
 
     print(board.get_state())
 
